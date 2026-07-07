380ef54a843e88065f10a818599ff3e42c1f5581
Singam Sravani <singamsravani410@gmail.com>
2026-07-07 17:53:25 +0530

    Data types

---
+++ operatorsexample.py
@@ -0,0 +1,60 @@
+#Arthmetic operators
+'''a=20 
+b=30
+print('addition', a+b)
+print('subtraction', a-b)
+print('multiplication', a*b)
+print('division', a/b)
+print('floor division', a//b)
+print('exponential', a**b)
+print('modulo division', a%b)'''
+
+# Assignment operators
+'''a=10
+a+=2
+print('assignment addition',a)
+a-=2
+print('assignment subtraction',a)
+a*=2
+print('assignment multiplication',a)
+a/=2
+print('assignment division',a)
+a//=2
+print('assignment floor division',a)
+a**=2
+print('assignment exponential',a)
+a%=2
+print('assignment modulo division',a)
+a=2
+print('assignment equal',a)'''
+
+
+#Relational/comparisional operators
+'''b=15
+c=6
+print(b<c)
+print(b>c)
+print(b==c)
+print(b<=c)
+print(b>=c)
+print(b!=c)'''
+
+
+
+'''S="SINGAM"
+print(S[:-1])
+print(S[-1:])
+print(S[::-1])
+print(S[-1:-1:-1])
+print(S[-1:2:-3])
+print(S[1:5:-1])
+print(S[-2:-5:-1]) '''
+
+'''x=7
+print(x>5 or x<3  and x==7)'''
+
+
+'''a=[1,2,3]
+b=a[:]
+print(a is b, a==b)'''
+print(15&7)

8e717b88701d4da1335b79226f6a8c37e9ab6603
Singam Sravani <singamsravani410@gmail.com>
2026-07-24 21:34:43 +0530

    List Comprehension

---
+++ project/Bank/main.py
@@ -0,0 +1,36 @@
+# DATA BASE
+"""
+users={
+        Account:{
+                'name':"Username,
+                'email':User email,
+                'balance':5000,
+                'password']:password
+                }
+        }
+        
+"""
+
+
+users={
+    1001:{'name':"Sravani",'email':singamsravani410@gmail.com,'balance':5000,'password']:'1001'}
+    1001:{'name':"Akhila",'email':Akhilashetty@gmail.com,'balance':4000,'password']:'1002'}
+}
+
+
+##Register function
+def register(username:str, email:str, balance:int, password:str)->str:
+    pass
+
+##Login Function
+def login(account:int,password:str)->bool:
+    pass
+
+## Get Balance
+def balance(account:int)->str:
+    pass
+
+#Withdraw
+def Withdraw(account:int)->str:
+    pass
+
